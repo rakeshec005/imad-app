af64bc5492d6160fe1237ced176103ac398d719f
rakesh005 <rak1994kumar@gmail.com>
2018-02-21 19:12:24 +0530

    sendFile

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', (req, res) => {
-  res.send(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', (req, res) => {
