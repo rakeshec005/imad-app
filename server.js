7904babd9ca119395d3eeefc0762d0f6f6f0bbef
rakesh005 <rak1994kumar@gmail.com>
2018-02-21 21:40:02 +0530

    added main.js path

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "ui", "index.html"));
 });
 
-// :articleName = express function(:) which convert in variable
+// :articleName = express function(:) which convert in variable (Url parametrization)
 app.get("/:articleName", (req, res) => {
   let articleName = req.params.articleName;  //provided by express framework
   res.send(createTemplate(articles[articleName]));
@@ -97,6 +97,10 @@ app.get("/:articleName", (req, res) => {
 
 app.get("/ui/style.css", function(req, res) {
   res.sendFile(path.join(__dirname, "ui", "style.css"));
+});
+
+app.get("/ui/main.js", function(req, res) {
+  res.sendFile(path.join(__dirname, "ui", "main.js"));
 });
 
 app.get("/ui/madi.png", function(req, res) {
