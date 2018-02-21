a99920dedb30038b3c27d3d4c9540e41caaff959
rakesh005 <rak1994kumar@gmail.com>
2018-02-21 22:57:48 +0530

    creating ajax request for counter

--- ui/main.js
+++ ui/main.js
@@ -2,15 +2,32 @@
 
 console.log('Loaded!');
 
-//move images
-let moveImage = document.getElementById('madi');
-let marginLeft = 0;
-function moveRight() {
-    marginLeft += 1;
-    moveImage.style.marginLeft = marginLeft + 'px';
-}
+//counter code
+let btn = document.getElementById('counter');
+btn.onclick = () => {
 
-moveImage.onclick = () => {
-    let interval = setInterval(moveRight, 50);
-    
-};
+    // Ajax calls
+    // create a request object
+    let xhr = new XMLHttpRequest();
+
+    // capture the response and store it in the variable
+    xhr.onreadystatechange = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            // Take some action
+            if(xhr.status === 200){
+                // extract the value from this request
+                let counter = xhr.responseText;
+
+                //render the variable in the correct span
+                let span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+ 
+    // Make the request
+    xhr.open('GET', 'http://localhost/counter', true);
+    xhr.send(null);
+   
+}
