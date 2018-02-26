5ad8db186f100d7fcf72a8b839a881855e61e5d1
rakesh005 <rak1994kumar@gmail.com>
2018-02-27 05:11:19 +0530

    creating request to server when submit btn clicked

--- ui/main.js
+++ ui/main.js
@@ -1,49 +1,66 @@
 //import { setInterval } from "timers";
 
-console.log('Loaded!');
+console.log("Loaded!");
 
 //counter code
-let btn = document.getElementById('counter');
+let btn = document.getElementById("counter");
 btn.onclick = () => {
+  // Ajax calls
+  // create a request object
+  let xhr = new XMLHttpRequest();
 
-    // Ajax calls
-    // create a request object
-    let xhr = new XMLHttpRequest();
+  // capture the response and store it in the variable
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Take some action
+      if (xhr.status === 200) {
+        // extract the value from this request
+        let counter = xhr.responseText;
 
-    // capture the response and store it in the variable
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            // Take some action
-            if(xhr.status === 200){
-                // extract the value from this request
-                let counter = xhr.responseText;
+        //render the variable in the correct span
+        let span = document.getElementById("count");
+        span.innerHTML = counter.toString();
+      }
+    }
+    // Not done yet
+  };
 
-                //render the variable in the correct span
-                let span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-        // Not done yet
-    };
- 
-    // Make the request
-    xhr.open('GET', 'http://localhost/counter', true);
-    xhr.send(null);
-   
-}
+  // Make the request
+  xhr.open("GET", "http://localhost/counter", true);
+  xhr.send(null);
+};
 
 // Submit name
-let nameInput = document.getElementById('name');
-let name = nameInput.value();
+let submit = document.getElementById("submit_btn");
 submit.onclick = () => {
-    // Make a request to server and send the name
+  // Make a request to server and send the name
+  // Ajax calls
+  // create a request object
+  let xhr = new XMLHttpRequest();
 
-    // capture a list of name and render it as a list
-    let names = ['name1', 'name2', 'name3', 'name4'];
-    let list = '';
-    for(let i=0; i<names.length; i++){
-        list += '<li>'+ names[i] + '</li>';
+  // capture the response and store it in the variable
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      // Take some action
+      if (xhr.status === 200) {
+        // capture a list of name and render it as a list
+        let names = JSON.parse(xhr.responseText);
+        let list = "";
+        for (let i = 0; i < names.length; i++) {
+          list += "<li>" + names[i] + "</li>";
+        }
+        let ul = document.getElementById("namelist");
+        ul.innerHTML = list;
+      }
     }
-    let ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-}
+    // Not done yet
+  };
+
+  // Extracting a input field value
+  let name = document.getElementById("name").value;
+  //let name = nameInput.value();
+
+  // Make the request
+  xhr.open("GET", "http://localhost/submit-name?name=" + name, true);
+  xhr.send(null);
+};
